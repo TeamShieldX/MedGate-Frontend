266c40f3c3d7dc2b299717ec2ac4ef5667a1acfd
SAMKIEL <samuelezekiel488@gmail.com>
2026-09-17 20:13:18 +0100

    feat(routes): wire complete application routes including landing and audit views

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
+import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import PatientListPage from './pages/PatientListPage';
 import PatientDetailPage from './pages/PatientDetailPage';
@@ -11,11 +12,12 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/patients" replace />} />
+          <Route index element={<LandingPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="patients" element={<PatientListPage />} />
           <Route path="patients/:id" element={<PatientDetailPage />} />
           <Route path="audit-log" element={<AuditLogPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
